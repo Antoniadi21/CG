9c49702985075ed0eb47ebf6df371b1738f3b3be
antoniadi nikita <nikitaantoniadi@gmail.com>
2023-06-06 17:30:08 +0300

    sueta 2

--- CG/Form1.cs
+++ CG/Form1.cs
@@ -23,7 +23,7 @@ namespace CG
         private void Form1_Load(object sender, EventArgs e)
         {
             Bitmap bitmap = new Bitmap(bitmapXSize, bitmapYSize);
-            drawEllipse(new Point(200, 300), 70, 120, bitmap, Color.Magenta);
+            drawEllipse(new Point(200, 250), 70, 120, bitmap, Color.Magenta);
             pictureBox1.Image = bitmap;
         }
 
@@ -31,24 +31,64 @@ namespace CG
 
         private void drawEllipse(Point p1, int h, int w, Bitmap bitmap, Color color)
         {
-            double angle = 0;
+            //double angle = 0;
 
-            while (angle < Math.PI / 2)
+            //while (angle < Math.PI / 2)
+            //{
+            //    int x = (int)(w * Math.Cos(angle));
+            //    int y = (int)(h * Math.Sin(angle));
+            //    drawPixels(bitmap, p1.X, p1.Y, x, y, color);
+            //    angle += Math.PI / 720.0;
+            //}
+
+            int x = 0;
+            int y = h;
+
+            int delta = 4 * h * h * (x + 1) * (x + 1) + w * w * (2 * y - 1) * (2 * y - 1) - 4 * w * w * h * h;
+            while (w * w * (2 * y - 1) > 2 * h * h * (x + 1))
             {
-                int x = (int)(w * Math.Cos(angle));
-                int y = (int)(h * Math.Sin(angle));
                 drawPixels(bitmap, p1.X, p1.Y, x, y, color);
-                angle += Math.PI / 720.0;
+                if (delta < 0)
+                {
+                    x++;
+                    delta += 4 * h * h * (2 * x + 3);
+                }
+                else
+                {
+                    x++;
+                    delta += -8 * w * w * (y - 1) + 4 * h * h * (2 * x + 3);
+                    y--;
+
+                }
             }
-;
+
+            delta = h * h * (2 * x + 1) * (2 * x + 1) + 4 * w * w * (y + 1) * (y + 1) - 4 * w * h;
+            while (y + 1 != 0)
+            {
+                drawPixels(bitmap, p1.X, p1.Y, x, y, color);
+                if (delta < 0)
+                {
+                    y--;
+                    delta += 4 * w * w * (2 * y + 3);
+                }
+                else
+                {
+                    y--;
+                    delta += -8 * h * h * (x + 1) + 4 * w * w * (2 * y + 3);
+                    x++;
+                }
+            }
         }
+
+
+
 
         private void drawPixels(Bitmap bitmap, int x0, int y0, int x, int y, Color color) 
         {
             bitmap.SetPixel(x0 + x, bitmapYSize - y0 + y, color);
             bitmap.SetPixel(x0 - x, bitmapYSize - y0 - y, color);
             bitmap.SetPixel(x0 + x, bitmapYSize - y0 - y, color);
-            bitmap.SetPixel(x0 - x, bitmapYSize - y0 + y, color );
+            bitmap.SetPixel(x0 - x, bitmapYSize - y0 + y, color);
         }
     }
 }
